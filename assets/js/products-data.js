85918fe68c84cf62ad842c2851a4be7659a6a7e6
Vinicius Gabriel <madara5004br@gmail.com>
2024-03-06 19:28:18 -0300

    primeiro commit

---
+++ assets/js/products-data.js
@@ -0,0 +1,64 @@
+const products = [
+    {
+        id: 1,
+        name: 'Violão',
+        price: 380.60,
+        image: 'img_1.jpg',
+        onCart: false
+    },
+    {
+        id: 2,
+        name: 'Guitarra',
+        price: 650,
+        image: 'img_2.jpg',
+        onCart: false
+    },
+    {
+        id: 3,
+        name: 'Contra-Baixo',
+        price: 1280,
+        image: 'img_3.jpg',
+        onCart: false
+    },
+    {
+        id: 4,
+        name: 'Contra-Baixo',
+        price: 1730,
+        image: 'img_4.jpg',
+        onCart: false
+    },
+    {
+        id: 5,
+        name: 'Piano',
+        price: 7032,
+        image: 'img_5.jpg',
+        onCart: false
+    },
+    {
+        id: 6,
+        name: 'Violino',
+        price: 450,
+        image: 'img_6.jpg',
+        onCart: false
+    },
+    {
+        id: 7,
+        name: 'Saxofone',
+        price: 6500,
+        image: 'img_7.jpg'
+    },
+    {
+        id: 8,
+        name: 'Banjo',
+        price: 630,
+        image: 'img_8.jpg',
+        onCart: false
+    },
+    {
+        id: 9,
+        name: 'Teclado',
+        price: 600,
+        image: 'img_9.jpg',
+        onCart: false
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+const mainContent = document.querySelector('.main-content');
+const span = document.querySelector('.button.cart > span');
+let productsOnCart = [];
+
+function toBRL(amout){
+    return amout.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+}
+
+function renderProducts(){
+    let htmlContent = '';
+
+    products.map(product => (
+        htmlContent += `
+        <div class="product-item">
+            <div class="product-img"><img src="assets/images/products/${product.image}" alt="${product.name}"></div>
+            <div class="product-name">${product.name}</div>
+            <div class="product-price">${toBRL(product.price)}</div>
+            <button onClick="onCartToggle(${product.id})" data-button="${product.id}" class="button">Adicionar ao carrinho</button>
+        </div>`
+    ));
+
+    mainContent.innerHTML = htmlContent;
+}
+
+function removeItemOnCart(id){
+    let newArray = productsOnCart.filter(productOnCart => productOnCart.id !== id);
+    productsOnCart = newArray;
+}
+
+function onCartToggle(id){
+    if(products.length > 0){
+        products.forEach(product => {
+            if(product.id === id){
+                product.onCart = !product.onCart;
+                productButtons.forEach(productButton => {
+                    if(parseInt(productButton.getAttribute('data-button')) === id){
+                        if(product.onCart){
+                            productButton.classList.add('item-on-cart');
+                            productButton.innerText = 'Adicionado ao Carrinho';
+                            productsOnCart.push(product);
+                            span.innerText = productsOnCart.length;
+                        }else{
+                            productButton.classList.remove('item-on-cart');
+                            productButton.innerText = 'Adicionar ao Carrinho';
+                            removeItemOnCart(product.id);
+                            span.innerText = productsOnCart.length;
+                        }
+                        console.log(productsOnCart);
+                    }
+                });
+            }
+        });
+    }
+}
+
+renderProducts();
+const productButtons = document.querySelectorAll('.main-content .button');
