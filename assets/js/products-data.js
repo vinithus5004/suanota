a58bbbd82aefaaecc88d4847d36f226e5e3d5f9b
Vinicius Gabriel <madara5004br@gmail.com>
2024-03-07 08:28:15 -0300

    funcionalidade de adicionar e remover itens do carrinho

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -4,42 +4,48 @@ const products = [
         name: 'Violão',
         price: 380.60,
         image: 'img_1.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 2,
         name: 'Guitarra',
         price: 650,
         image: 'img_2.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 3,
         name: 'Contra-Baixo',
         price: 1280,
         image: 'img_3.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 4,
         name: 'Contra-Baixo',
         price: 1730,
         image: 'img_4.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 5,
         name: 'Piano',
         price: 7032,
         image: 'img_5.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 6,
         name: 'Violino',
         price: 450,
         image: 'img_6.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 7,
@@ -52,13 +58,15 @@ const products = [
         name: 'Banjo',
         price: 630,
         image: 'img_8.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     },
     {
         id: 9,
         name: 'Teclado',
         price: 600,
         image: 'img_9.jpg',
-        onCart: false
+        onCart: false,
+        qtOnCart: 0
     }
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const mainContent = document.querySelector('.main-content');
 const span = document.querySelector('.button.cart > span');
 
 let productsOnCart = [];
+let qtElements = []
 
 function toBRL(amout){
     return amout.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
@@ -38,15 +39,16 @@ function onCartToggle(id){
                         if(product.onCart){
                             productButton.classList.add('item-on-cart');
                             productButton.innerText = 'Adicionado ao Carrinho';
-                            productsOnCart.push(product);
+                            productsOnCart.push({...product, qtOnCart: 1});
                             span.innerText = productsOnCart.length;
+                            renderProductsOnCart();
                         }else{
                             productButton.classList.remove('item-on-cart');
                             productButton.innerText = 'Adicionar ao Carrinho';
                             removeItemOnCart(product.id);
                             span.innerText = productsOnCart.length;
+                            renderProductsOnCart();
                         }
-                        console.log(productsOnCart);
                     }
                 });
             }
@@ -62,6 +64,7 @@ const modalContainer = document.querySelector('.modal-container');
 const openModal = document.querySelector('.button.cart');
 const closeModal = document.querySelector('.modal-header .close-modal');
 const productsCount = document.querySelector('.modal-header span');
+const productsElement = document.querySelector('.modal .products');
 
 openModal.addEventListener('click', () => {
     productsCount.innerText = `${productsOnCart.length} ${productsOnCart.length === 1 ? 'item adicionado' : 'itens adicionados'} ao carrinho`;
@@ -71,3 +74,56 @@ openModal.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
     modalContainer.classList.remove('active');
 })
+
+function renderProductsOnCart(){
+    let htmlContent = '';
+
+    productsOnCart.map(product => (
+        htmlContent += `
+        <div class="product-item">
+            <div class="product-img"><img src="assets/images/products/${product.image}" alt=""></div>
+            <div class="item-info-container">
+                <div class="item-info">
+                    <div class="product-name">${product.name}</div>
+                    <div class="product-price">${toBRL(product.price)}</div>
+                    <div class="product-count">
+                        <span data-content="${product.id}">Quantidade: ${product.qtOnCart}x</span>
+                        <i class="fa-solid fa-plus" onClick="increase(${product.id})"></i>
+                        <i class="fa-solid fa-minus" onClick="decrease(${product.id})"></i>
+                    </div>
+                </div>
+                <button class="button">Comprar</button>
+            </div>
+        </div>`
+    ));
+
+    productsElement.innerHTML = htmlContent;
+    qtElements = document.querySelectorAll('.modal .products span');
+}
+
+function increase(id){
+    productsOnCart.forEach(product => {
+        if(product.id === id){
+            product.qtOnCart++;
+            qtElements.forEach(qtElement => {
+                if(parseInt(qtElement.getAttribute('data-content')) === id) qtElement.innerText = `Quantidade: ${product.qtOnCart}x`;
+            });
+        }
+    });
+}
+function decrease(id){
+    productsOnCart.forEach(product => {
+        if(product.id === id){
+            if(product.qtOnCart > 0){
+                product.qtOnCart--;
+                qtElements.forEach(qtElement => {
+                    if(parseInt(qtElement.getAttribute('data-content')) === id) qtElement.innerText = `Quantidade: ${product.qtOnCart}x`;
+                });
+            }
+            if(product.qtOnCart === 0){
+                onCartToggle(product.id);
+                productsCount.innerText = `${productsOnCart.length} ${productsOnCart.length === 1 ? 'item adicionado' : 'itens adicionados'} ao carrinho`;
+            }
+        }
+    });
+}
