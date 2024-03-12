22decfe7c2f786cf2ec889b561d9636aebc97e6d
Vinicius Gabriel <madara5004br@gmail.com>
2024-03-12 17:01:23 -0300

    finalizado e responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ const mainContent = document.querySelector('.main-content');
 const span = document.querySelector('.button.cart > span');
 
 let productsOnCart = [];
-let qtElements = []
+let qtElements = [];
+let totalAmount = 0;
 
 function toBRL(amout){
     return amout.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
@@ -66,13 +67,19 @@ const closeModal = document.querySelector('.modal-header .close-modal');
 const productsCount = document.querySelector('.modal-header span');
 const productsElement = document.querySelector('.modal .products');
 
+const totalAmountElement = document.querySelector('.total-amount');
+const emptyCart = document.querySelector('.empty-cart');
+const finalizePurchase = document.querySelector('.finalize-purchase');
+
 openModal.addEventListener('click', () => {
     productsCount.innerText = `${productsOnCart.length} ${productsOnCart.length === 1 ? 'item adicionado' : 'itens adicionados'} ao carrinho`;
     modalContainer.classList.add('active');
+    document.querySelector('body').style.overflow = 'hidden';
 });
 
 closeModal.addEventListener('click', () => {
     modalContainer.classList.remove('active');
+    document.querySelector('body').style.overflow = 'auto';
 })
 
 function renderProductsOnCart(){
@@ -82,23 +89,32 @@ function renderProductsOnCart(){
         htmlContent += `
         <div class="product-item">
             <div class="product-img"><img src="assets/images/products/${product.image}" alt=""></div>
-            <div class="item-info-container">
-                <div class="item-info">
-                    <div class="product-name">${product.name}</div>
-                    <div class="product-price">${toBRL(product.price)}</div>
-                    <div class="product-count">
-                        <span data-content="${product.id}">Quantidade: ${product.qtOnCart}x</span>
-                        <i class="fa-solid fa-plus" onClick="increase(${product.id})"></i>
-                        <i class="fa-solid fa-minus" onClick="decrease(${product.id})"></i>
-                    </div>
+            <div class="item-info">
+                <div class="product-name">${product.name}</div>
+                <div class="product-price">${toBRL(product.price)}</div>
+                <div class="product-count">
+                    <span data-content="${product.id}">Quantidade: ${product.qtOnCart}x</span>    
                 </div>
-                <button class="button">Comprar</button>
+                <div class="buttons-controls">
+                    <i class="fa-solid fa-plus" onClick="increase(${product.id})"></i>
+                    <i class="fa-solid fa-minus" onClick="decrease(${product.id})"></i>
+                    <span class="remove-from-cart" onClick=(removeFromCart(${product.id}))>Remover</span>
+                </div>
             </div>
         </div>`
     ));
 
     productsElement.innerHTML = htmlContent;
     qtElements = document.querySelectorAll('.modal .products span');
+    calculateTotalAmount();
+
+    if(productsOnCart.length > 0){
+        emptyCart.classList.remove('visible');
+        finalizePurchase.classList.add('visible');
+    }else{
+        emptyCart.classList.add('visible');
+        finalizePurchase.classList.remove('visible');
+    }
 }
 
 function increase(id){
@@ -110,6 +126,7 @@ function increase(id){
             });
         }
     });
+    calculateTotalAmount();
 }
 function decrease(id){
     productsOnCart.forEach(product => {
@@ -126,4 +143,18 @@ function decrease(id){
             }
         }
     });
+    calculateTotalAmount();
+}
+
+function calculateTotalAmount(){
+    let total = 0;
+    productsOnCart.map(product => total += (product.price * product.qtOnCart));
+
+    totalAmount = total;
+    totalAmountElement.innerHTML = `<div class="total-amout">Total: ${toBRL(totalAmount)}</div>`
+}
+
+function removeFromCart(id){
+    onCartToggle(id);
+    productsCount.innerText = `${productsOnCart.length} ${productsOnCart.length === 1 ? 'item adicionado' : 'itens adicionados'} ao carrinho`;
 }
